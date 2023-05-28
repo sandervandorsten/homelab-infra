3d8e48e64889c5e77324fc9ac23c95be14f03048
Sander van Dorsten <sander.vandorsten@digital-power.com>
2023-05-28 19:12:25 +0200

    feat: put docker server in module

---
+++ terraform/modules/docker_server/main.tf
@@ -0,0 +1,46 @@
+resource "proxmox_vm_qemu" "ubuntu_docker_server" {
+
+  # VM General Settings
+  target_node = var.node
+  vmid        = var.vm_id
+  name        = var.vm_name
+  desc        = var.vm_description
+
+  # VM Advanced General Settings
+  onboot = true
+
+  # VM OS Settings (name of the vm template to clone from)
+  clone = var.vm_template_name
+
+  # VM System Settings
+  # Needs to be set to increase deployment time according to Christian Lempa
+  agent = 1
+
+  # VM CPU Settings
+  cores   = var.cores
+  sockets = 1
+  cpu     = "host"
+
+  # VM Memory Settings
+  memory = var.memory
+
+  # VM Network Settings
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  # VM Cloud-Init Settings
+  os_type = "cloud-init"
+
+  # IP Address and Gateway
+  ipconfig0 = "ip=${var.vm_ip_address}/24,gw=${var.gateway_ip}"
+
+  # User for login
+  ciuser = var.ciuser
+
+  # Add your SSH KEYs separated by newlines
+  sshkeys = <<EOF
+    ${var.ssh_key_public}
+  EOF
+}
---
+++ terraform/modules/docker_server/variables.tf
@@ -0,0 +1,66 @@
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+}
+
+variable "proxmox_api_token_secret" {
+  type = string
+}
+
+variable "ssh_key_public" {
+  type        = string
+  description = "Public key of the SSH key used for logging into the Virtual Machines"
+}
+
+variable "node" {
+  type        = string
+  description = "Name of the VM Host Node in Proxmox"
+}
+
+variable "vm_id" {
+  type        = string
+  description = "VM ID (number) of the guest node."
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Name of the VM Node"
+}
+
+variable "vm_description" {
+  type        = string
+  default     = "Ubuntu server with docker installed"
+  description = "Description of the template"
+}
+
+variable "vm_ip_address" {
+  type        = string
+  description = "IP Address of the ubuntu docker VM on the local network"
+}
+
+variable "gateway_ip" {
+  type        = string
+  description = "IP Address of the Gateway of the internal Network."
+}
+
+variable "vm_template_name" {
+  type        = string
+  description = "Name of the Proxmox VM Template to clone the VM from"
+}
+
+variable "cores" {
+  type        = number
+  description = "Number of cores to use"
+}
+variable "memory" {
+  type        = number
+  description = "Memory assigned (MB)"
+}
+variable "ciuser" {
+  type        = string
+  description = "User to login to on the VM"
+  default     = "sander"
+}
