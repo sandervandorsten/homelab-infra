225370aeba311e8451b149085eed815c8a70661f
Sander van Dorsten <sander.vandorsten@digital-power.com>
2023-05-28 19:12:25 +0200

    feat: put docker server in module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,16 @@
-
-resource "proxmox_vm_qemu" "ubuntu_docker_server" {
-
-  # VM General Settings
-  target_node = var.node
-  vmid        = var.vm_id
-  name        = var.vm_name
-  desc        = var.vm_description
-
-  # VM Advanced General Settings
-  onboot = true
-
-  # VM OS Settings (name of the vm template to clode from)
-  clone = "ubuntu-docker-template"
-
-  # VM System Settings
-  agent = 1
-
-  # VM CPU Settings
-  cores   = 1
-  sockets = 1
-  cpu     = "host"
-
-  # VM Memory Settings
-  memory = 2048
-
-  # VM Network Settings
-  network {
-    bridge = "vmbr0"
-    model  = "virtio"
-  }
-
-  # VM Cloud-Init Settings
-  os_type = "cloud-init"
-
-  # IP Address and Gateway
-  ipconfig0 = "ip=${var.vm_ip_address}/24,gw=${var.gateway_ip}"
-
-  # User for login
-  ciuser = "sander"
-
-  # (Optional) Add your SSH KEY
-  sshkeys = <<EOF
-    ${var.ssh_key_public}
-  EOF
+module "docker-server" {
+  source                   = "./modules/docker_server"
+  proxmox_api_token_id     = var.proxmox_api_token_id
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+  proxmox_api_url          = var.proxmox_api_url
+  ssh_key_public           = var.ssh_key_public
+  node                     = var.node
+  vm_id                    = 104
+  vm_name                  = "ubuntu-docker"
+  vm_ip_address            = var.vm_ip_address
+  gateway_ip               = var.gateway_ip
+  vm_template_name         = "ubuntu-docker-template"
+  cores                    = 1
+  memory                   = 2048
+  ciuser                   = "sander"
 }
