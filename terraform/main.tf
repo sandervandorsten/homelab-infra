7670ae5e9b6a34943d84822f1843d4ad37eeb47a
Sander van Dorsten <sander.vandorsten@digital-power.com>
2023-05-28 19:36:27 +0200

    feat: bundled terraform config for docker vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,12 @@ module "docker-server" {
   proxmox_api_url          = var.proxmox_api_url
   ssh_key_public           = var.ssh_key_public
   node                     = var.node
-  vm_id                    = 104
-  vm_name                  = "ubuntu-docker"
-  vm_ip_address            = var.vm_ip_address
   gateway_ip               = var.gateway_ip
-  vm_template_name         = "ubuntu-docker-template"
-  cores                    = 1
-  memory                   = 2048
-  ciuser                   = "sander"
+  vm_id                    = var.docker-server-config.vm_id
+  vm_name                  = var.docker-server-config.vm_name
+  vm_ip_address            = var.docker-server-config.vm_ip_address
+  vm_template_name         = var.docker-server-config.vm_template_name
+  cores                    = var.docker-server-config.cores
+  memory                   = var.docker-server-config.memory
+  ciuser                   = var.docker-server-config.ciuser
 }
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -1,4 +1,10 @@
-gateway_ip    = "192.168.178.1"
-vm_id         = 103
-vm_name       = "ubuntu-docker"
-vm_ip_address = "192.168.178.12"
+gateway_ip = "192.168.178.1"
+docker-server-config = {
+  vm_id            = "100"
+  vm_name          = "ubuntu-docker"
+  vm_ip_address    = "192.168.178.12"
+  vm_template_name = "ubuntu-docker-template"
+  cores            = 1
+  memory           = 2048
+  ciuser           = "sander"
+}
